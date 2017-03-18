98dfc6698cf3230ce8f270c42f141a9a1aed9e18
shrutishirol <shrutishirol3@gmail.com>
2017-03-18 20:06:33 +0530

    initial commit

---
+++ GridViewImages4.3/app/src/main/java/com/mobilerepair/app/gridviewimages43/GridAdapter.java
@@ -0,0 +1,60 @@
+package com.mobilerepair.app.gridviewimages43;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by Santoshraddi on 21-02-2017.
+ */
+public class GridAdapter extends BaseAdapter {
+
+    private Context mContext;
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.gingerbread, R.drawable.honeycomb,
+            R.drawable.icecream, R.drawable.jellybean,
+            R.drawable.kitkat, R.drawable.lollipop
+
+    };
+
+    public GridAdapter(Context c) {
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+}
---
+++ GridViewImages4.3/app/src/main/java/com/mobilerepair/app/gridviewimages43/MainActivity.java
@@ -0,0 +1,39 @@
+package com.mobilerepair.app.gridviewimages43;
+
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GridView gridView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new GridAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
